a54dec32551b67734d3cbd1ad768337285373adf
ronaldr <ronald.rahukyla@gmail.com>
2024-11-22 05:50:04 +0200

    UntilCocktails

---
+++ ShopTARge23/ShopTARge23.Core/ServiceInterface/ICocktailServices.cs
@@ -0,0 +1,11 @@
+﻿using ShopTARge23.Core.Dto.CocktailDto;
+
+
+
+namespace ShopTARge23.Core.ServiceInterface
+{
+    public interface ICocktailServices
+    {
+        Task<CocktailResultDto> GetCocktails(CocktailResultDto dto);
+    }
+}
